68607b24c3be454684fe179d9dabde85613b34e5
KrystianFH <krystianfrancuz@gmail.com>
2020-09-15 16:05:25 -0700

    code challenge 2

--- challenges/src/main/java/challenges/ArrayShift.java
+++ challenges/src/main/java/challenges/ArrayShift.java
@@ -1,4 +1,29 @@
 package challenges;
 
+import java.util.Arrays;
+
 public class ArrayShift {
-}
+
+        public int[] insertShiftArray(int[] arr, int val) {
+            int[] numbers = new int[]{2, 4, 6, 8};
+            int newNumb = 5;
+            int[] newNumbers = new int[numbers.length + 1];
+            for (int i = newNumbers.length - 1; i > 0; i++) {
+                if (newNumbers[i] == 0) continue;
+//                numbers[i + 1] = numbers[i];
+                if (newNumbers[i] <= val) {
+                    arr[i] = val;
+                    break;
+                }
+//            int[] objArr = add(numbers, newNumb);
+
+                System.out.println(Arrays.toString(newNumbers));
+                return newNumbers;
+            }
+            return newNumbers;
+        }}
+
+
+
+
+
--- challenges/src/test/java/challenges/ArrayShiftTest.java
+++ challenges/src/test/java/challenges/ArrayShiftTest.java
@@ -1,4 +1,19 @@
 package challenges;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+
 public class ArrayShiftTest {
+
+    @Test
+    public void testInsertShiftArray() {
+        ArrayShift classUnderTest = new ArrayShift();
+        int[] inputArray1 = new int[]{2, 4, 6, 8};
+        int[] outputArray1 = classUnderTest.insertShiftArray(inputArray1, 5);
+        int[] expectedOutcome = {2, 4, 5, 6, 8};
+
+        assertArrayEquals(expectedOutcome, outputArray1);
+    }
 }
