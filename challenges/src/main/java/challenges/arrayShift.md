30d8659ab6bd763f8a91ffe3cc8c8cbf878239b2
KrystianFH <krystianfrancuz@gmail.com>
2020-09-16 15:14:53 -0700

    created binary search library, completed cc3, refactored root readme and created readme files for each code challenge

---
+++ challenges/src/main/java/challenges/arrayShift.md
@@ -0,0 +1,13 @@
+### Class 02 - Array Shift
+
+# Insert and Shift an Array
+Insert and shift array in middle at index
+
+## Challenge
+Write a function called insertShiftArray which takes an array as an argument and the value to be added, return an array with the new value added at the middle index.
+
+## Approach & Efficiency
+My approach was to Google references to use for inserting a value into a sorted array. 
+
+## Solution
+![Insert and Shift an Array Whiteboard](../../resources/whiteboardCodeChallenge2.jpg)
---
+++ challenges/src/test/java/challenges/BinarySearchTest.java
@@ -0,0 +1,22 @@
+package challenges;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class BinarySearchTest {
+    @Test public void testBinarySearch() {
+        BinarySearch test = new BinarySearch();
+
+        int[] testArr1 = new int[]{2, 4, 6, 8, 10};
+        int searchKey1 = 8;
+
+        int[] testArr2 = new int[]{1, 2, 3, 4, 5, 7, 8, 9, 10};
+        int searchKey2 = 6;
+
+        assertEquals("Success, you've found the key!", test.binarySearch(testArr1, searchKey1), 3);
+        assertEquals("No key for you mate", test.binarySearch(testArr2, searchKey2), -1);
+
+    }
+}
