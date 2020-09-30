24ab3fc0d554d7e7b9fd19d39c9f59076d6d135c
KrystianFH <krystianfrancuz@gmail.com>
2020-09-30 14:43:47 -0700

    worked on CC13 and added in whiteboard

---
+++ challenges/src/test/java/challenges/utilitiesTest/MultiBracketValidationTest.java
@@ -0,0 +1,37 @@
+package challenges.utilitiesTest;
+
+
+import challenges.utilities.MultiBracketValidation;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.Stack;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MultiBracketValidationTest {
+
+    @Test
+    public void multiBracketValidationTest() {
+
+        String oneT = "{}"; //True
+        String twoT = "{}(){}"; //True
+        String threeT = "()[[Extra Characters]]"; //True
+        String fourT = "(){}[[]]"; //True
+        String fiveT = "{}{Code}[Fellows](())"; //True
+        String oneF = "[({}]"; //False
+        String twoF = "(]("; //False
+        String threeF = "{(})"; //False
+
+
+        assertEquals("This string is true", true, MultiBracketValidation.multiBracketValidation(oneT));
+        assertEquals("This string is true", true, MultiBracketValidation.multiBracketValidation(twoT));
+        assertEquals("This string is true", true, MultiBracketValidation.multiBracketValidation(threeT));
+        assertEquals("This string is true", true, MultiBracketValidation.multiBracketValidation(fourT));
+        assertEquals("This string is true", true, MultiBracketValidation.multiBracketValidation(fiveT));
+        assertEquals("This string is false", false, MultiBracketValidation.multiBracketValidation(oneF));
+        assertEquals("This string is false", false, MultiBracketValidation.multiBracketValidation(oneF));
+        assertEquals("This string is false", false, MultiBracketValidation.multiBracketValidation(oneF));
+    }
+}
